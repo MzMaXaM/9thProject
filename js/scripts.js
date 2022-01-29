84c20fcefca890775ab5be38b4296c8cb3fe34b1
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-01-29 21:38:59 +0000

    added posibility to edit player names

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-function showForm(){
+function showForm(event){
   backdrop.style.display = 'block'
   asideForm.style.display = 'block'
+  playerEdit = +event.target.dataset.playerid
+
 }
 
 function hideForm(){
   backdrop.style.display = 'none'
   asideForm.style.display = 'none'
+  errorMsg.innerText = ''
+  playerEdit = 0
+  playerNameInput.value = ''
 }
 
 function savePlayerName(event){
   event.preventDefault()
   const formData = new FormData(event.target)
   const enteredPlayerName = formData.get('username').trim()
-  console.log(enteredPlayerName)
+
+  if(!enteredPlayerName){
+    errorMsg.innerText = 'Enter a NickName please.'
+    return
+  }
+
+  if(playerEdit > 0){
+    if(playerEdit === 1) {
+      firstPlayerName.innerText = enteredPlayerName
+      firstPlayer.name = enteredPlayerName
+    }else {
+      secondPlayerName.innerText = enteredPlayerName
+      secondPlayer.name = enteredPlayerName
+    }
+  }
+
+  hideForm()
 }
--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,5 @@
+let playerEdit = 0
+
 const btnPlayer1Edit = document.getElementById('player1EditBtn')
 const btnPlayer2Edit = document.getElementById('player2EditBtn')
 const btnCancelForm = document.getElementById('cancelForm')
@@ -7,6 +9,22 @@ const btnStartGame = document.getElementById('startGame')
 const form = document.querySelector('form')
 const backdrop = document.getElementById('backdrop')
 const asideForm = document.querySelector('aside')
+const errorMsg = document.getElementById('errorMessage')
+const playerNameInput = document.getElementById('playerNameInput')
+
+const firstPlayer = {
+  name:'',
+  symbol:'X',
+  key:1
+}
+const secondPlayer = {
+  name:'',
+  symbol:'O',
+  key:2
+}
+
+let firstPlayerName = document.querySelector('#firstPlayerField h3')
+let secondPlayerName = document.querySelector('#secondPlayerField h3')
 
 btnPlayer1Edit.addEventListener('click', showForm)
 btnPlayer2Edit.addEventListener('click', showForm)
