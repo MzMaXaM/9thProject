fcef10bfacf29bfba1a89cf272f9de2108f3a7f3
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-01-30 22:46:12 +0000

    we can start a new game, the right symbol is added on the field.

--- js/scripts.js
+++ js/scripts.js
@@ -25,13 +25,42 @@ function savePlayerName(event){
 
   if(playerEdit > 0){
     if(playerEdit === 1) {
-      firstPlayerName.innerText = enteredPlayerName
       firstPlayer.name = enteredPlayerName
+      firstPlayerName.innerText = firstPlayer.name
     }else {
-      secondPlayerName.innerText = enteredPlayerName
       secondPlayer.name = enteredPlayerName
+      secondPlayerName.innerText = secondPlayer.name
     }
   }
 
   hideForm()
+}
+
+
+function startNewGame(){
+  // if(firstPlayer.name === '' || secondPlayer.name === ''){
+  //   return
+  // }
+  if(firstPlayer.name === '' ) firstPlayer.name = 'NoName1'
+  if(secondPlayer.name === '' ) secondPlayer.name = 'NoName2'
+  firstPlayerName.innerText = firstPlayer.name
+  secondPlayerName.innerText = secondPlayer.name
+  activePlayerName.innerText = firstPlayer.name
+
+  gameBoard.style.display = 'grid'
+
+  btnStartGame.style.display = 'none'
+  activePlayerMessage.style.display = 'block'
+}
+
+function playerTurn(){
+  activePlayer === 1? activePlayer = 2:activePlayer = 1
+  activePlayer === 1? activePlayerName.innerText = firstPlayer.name :
+  activePlayerName.innerText = secondPlayer.name
+  activePlayer === 1? fieldSymbol = 'X': fieldSymbol = 'O'
+}
+function fieldClick(event){
+  if(event.target.textContent) return
+  event.target.textContent = fieldSymbol
+  playerTurn()
 }
--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,7 @@
 let playerEdit = 0
+let activePlayer = 1
+let fieldSymbol = 'X'
+let activePlayerName = document.getElementById('activePlayerName')
 
 const btnPlayer1Edit = document.getElementById('player1EditBtn')
 const btnPlayer2Edit = document.getElementById('player2EditBtn')
@@ -11,6 +14,8 @@ const backdrop = document.getElementById('backdrop')
 const asideForm = document.querySelector('aside')
 const errorMsg = document.getElementById('errorMessage')
 const playerNameInput = document.getElementById('playerNameInput')
+const gameBoard = document.getElementById('gameBoard')
+const activePlayerMessage = document.getElementById('activePlayerMessage')
 
 const firstPlayer = {
   name:'',
@@ -26,8 +31,15 @@ const secondPlayer = {
 let firstPlayerName = document.querySelector('#firstPlayerField h3')
 let secondPlayerName = document.querySelector('#secondPlayerField h3')
 
+const fieldLists = document.querySelectorAll('#gameBoard li')
+
 btnPlayer1Edit.addEventListener('click', showForm)
 btnPlayer2Edit.addEventListener('click', showForm)
 btnCancelForm.addEventListener('click', hideForm)
 backdrop.addEventListener('click', hideForm)
 form.addEventListener('submit', savePlayerName)
+btnStartGame.addEventListener('click', startNewGame)
+
+for (const fieldList of fieldLists) {
+  fieldList.addEventListener('click', fieldClick)
+}
